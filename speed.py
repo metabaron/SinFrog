71ed450ec275f368746ed1243bb03a4574a73396
Mathieu Feulvarch <metbaron@gmail.com>
2012-05-23 12:01:13 +0800

    Now, the list of files to download is retrieved from the server each week

--- speed.py
+++ speed.py
@@ -1,10 +1,10 @@
-import time, threading, urllib2, json
+import time, threading, urllib2, json, os, fileinput
+from datetime import datetime, timedelta
 
 class SpeedClass(threading.Thread):
     def report(start, size, text, displayTarget):
         total = time.time() - start
         displayTarget.DisplayCtrl.AppendText("\n" + "%s reading took %d seconds, transfer rate %.2f KBPS" % (text, total, (size / 1024.0) / total))
-        #print "%s reading took %d seconds, transfer rate %.2f KBPS" % (text, total, (size / 1024.0) / total)
 
     def __init__(self, displayTarget = None,  user = None):
         self.user = user
@@ -15,15 +15,33 @@ class SpeedClass(threading.Thread):
         #http://www.thinkbroadband.com/download.html
         #http://www.speedtest.com.sg/speedtest.php
         self.displayTarget.DisplayFrame_statusbar.SetStatusText("Download speed: Start")
-        urlList = ["http://ipv4.download.thinkbroadband.com/5MB.zip", "http://www.speedtest.com.sg/test_random_10mb.zip"]
-        for urlItem in urlList:
-            self.test(urlItem)
+        
+        if(os.path.exists(r'speedList.txt')):
+            statinfo = os.stat('speedList.txt')
+        else:
+            u = urllib2.urlopen('http://sinfrog.metabaron.net/speedList.php')
+            localFile = open('speedList.txt', 'w')
+            localFile.write(u.read())
+            localFile.close()
+            statinfo = os.stat('speedList.txt')
+        if (datetime.fromtimestamp(time.time()) - datetime.fromtimestamp(statinfo.st_mtime)) > timedelta (days = 7):
+            #Retrieve list of servers to test download speed from
+            u = urllib2.urlopen('http://sinfrog.metabaron.net/speedList.php')
+            localFile = open('speedList.txt', 'w')
+            localFile.write(u.read())
+            localFile.close()
+        #loading file line by line and not at once in memory
+        for line in fileinput.input(['speedList.txt']):
+        #urlList = ["http://ipv4.download.thinkbroadband.com/5MB.zip", "http://www.speedtest.com.sg/test_random_10mb.zip"]
+            self.test(line.strip(' \t\n\r'))
         self.displayTarget.DisplayFrame_statusbar.SetStatusText("Download speed: Done")
     
     def test(self, url):
         self.url = url
         f = urllib2.urlopen(self.url)
+        print("ok")
         start = time.time()
+        print("ok")
         
         self.displayTarget.DisplayCtrl.AppendText("\n" + "Chuncked reading from " + self.url)
         data = ''
